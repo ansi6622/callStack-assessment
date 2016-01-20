cd08d93bf55af45c3412b1378f34283edba94192
Kim Schlesinger <schlesinger.kim@gmail.com>
2016-01-20 09:34:04 -0700

    initial commit

---
+++ callStackAssessment.js
@@ -0,0 +1,36 @@
+function isPasswordStrong(password) {
+  var passwordLength = checkLength(password)
+  if (passwordLength < 12) {
+    return false;
+  }
+  if (isSimpleWord(password)) {
+    //third check here
+    return false;
+  }
+  if (hasOneSpecialChar(password)) {
+    return true;
+  }
+  return true;
+}
+
+function checkLength(string) {
+  //first check here
+  return string.length;
+}
+
+function isSimpleWord (string) {
+  var wordList = ["apple", "ball", "cat"]
+  if (wordList.indexOf(string) === -1) {
+    return false;
+  }
+  return true;
+}
+
+function hasOneSpecialChar(string) {
+  //second check here
+  var newArr = string.split('')
+  if (['!', '@', '#', '$'].indexOf(newArr) === -1) {
+    return false;
+  }
+  return true;
+}
